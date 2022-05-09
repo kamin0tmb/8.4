a9eec004cdfdafa7124ec7df48e4138085d68252
kamin0 <jm187@yandex.ru>
2022-05-09 17:02:13 +0300

    8.4.1

--- Program.cs
+++ Program.cs
@@ -3,54 +3,54 @@ using System.IO;
 
 class BinaryExperiment
 {
-    const string SettingsFileName = "Settings.cfg";
+    const string FileName = @"C:\Users\kamin0\Desktop\BinaryFile.bin";
 
     static void Main()
     {
         // Пишем
-        WriteValues();
+        //WriteValues();
         // Считываем
         ReadValues();
     }
 
-    static void WriteValues()
-    {
-        // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
-        using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Create)))
-        {
-            // записываем данные в разном формате
-            writer.Write(20.666F);
-            writer.Write(@"Тестовая строка");
-            writer.Write(55);
-            writer.Write(false);
-        }
-    }
+   // static void WriteValues()
+   // {
+   //    // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+   //     using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Create)))
+   //     {
+   //         // записываем данные в разном формате
+   //         writer.Write(20.666F);
+   //         writer.Write(@"Тестовая строка");
+   //         writer.Write(55);
+   //         writer.Write(false);
+   //     }
+   // }
 
     static void ReadValues()
     {
-        float FloatValue;
+        //float FloatValue;
         string StringValue;
-        int IntValue;
-        bool BooleanValue;
+        //int IntValue;
+        //bool BooleanValue;
 
-        if (File.Exists(SettingsFileName))
+        if (File.Exists(FileName))
         {
             // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
-            using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+            using (BinaryReader reader = new BinaryReader(File.Open(FileName, FileMode.Open)))
             {
                 // Применяем специализированные методы Read для считывания соответствующего типа данных.
-                FloatValue = reader.ReadSingle();
+                //FloatValue = reader.ReadSingle();
                 StringValue = reader.ReadString();
-                IntValue = reader.ReadInt32();
-                BooleanValue = reader.ReadBoolean();
+                //IntValue = reader.ReadInt32();
+                //BooleanValue = reader.ReadBoolean();
             }
 
             Console.WriteLine("Из файла считано:");
 
-            Console.WriteLine("Дробь: " + FloatValue);
+            //Console.WriteLine("Дробь: " + FloatValue);
             Console.WriteLine("Строка: " + StringValue);
-            Console.WriteLine("Целое: " + IntValue);
-            Console.WriteLine("Булево значение " + BooleanValue);
+            //Console.WriteLine("Целое: " + IntValue);
+            //Console.WriteLine("Булево значение " + BooleanValue);
         }
     }
 }
